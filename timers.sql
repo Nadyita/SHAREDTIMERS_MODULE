c57726b90a54bd9bc7675101d0ebc56a5873e48c
Nadyita <nadyita@hodorraid.org>
2019-10-30 09:18:57 +0100

    Initial release

---
+++ timers.sql
@@ -0,0 +1,1 @@
+CREATE TABLE IF NOT EXISTS `timers` (`name` VARCHAR(255), `owner` VARCHAR(25), `mode` VARCHAR(50), `endtime` int, `settime` int, `callback` VARCHAR(255), `data` VARCHAR(255), `alerts` TEXT);
